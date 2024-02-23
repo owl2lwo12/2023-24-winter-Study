7f085a57a2ecceb67e52a4dc1921832f2ad0179c
owl2lwo12 <owl2lwo12@ajou.ac.kr>
2024-02-23 21:21:25 +0900

    2-24 TIL

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### 2024/1/12 [TIL](https://github.com/owl2lwo12/2023-24-winter-Study/tree/main/WinterStudy/24-1-12)
 ### 2024/1/19 [TIL](https://github.com/owl2lwo12/2023-24-winter-Study/blob/main/WinterStudy/24-1-19)
 ### 2024/2/5 [TIL](https://github.com/owl2lwo12/2023-24-winter-Study/tree/main/WinterStudy/24-2-5)
+### 2024/2/23 [TIL](https://github.com/owl2lwo12/2023-24-winter-Study/tree/main/WinterStudy/24-2-23)
---
+++ WinterStudy/24-2-23/BJ18870.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	//input
+	int n;
+	cin >> n;
+
+	vector<int> nums; //모든 숫자를 저장
+	vector<int> sequences; // 모든 '종류'의 숫자를 '순서'대로 저장
+
+	for (int i = 0; i < n; i++) {
+		int t;
+		cin >> t;
+
+		nums.push_back(t);
+		sequences.push_back(t);
+	}
+	
+	sort(sequences.begin(), sequences.end());
+	sequences.erase(unique(sequences.begin(), sequences.end()), sequences.end());
+
+	for (int i = 0; i < n; i++) {
+		
+		cout << lower_bound(sequences.begin(),sequences.end(),nums[i]) - sequences.begin() << " ";
+	}
+}
